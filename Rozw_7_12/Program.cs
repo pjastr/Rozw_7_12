bd8bc4781ed4beb1168ee72acc072932c0cabd1c
Piotrek <@>
2017-11-29 14:46:39 +0100

    przeslanie plikow

---
+++ Rozw_7_12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Rozw_7_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Directory.Exists("ABC"))
+            {
+                if (File.Exists("ABC\\bilet.txt"))
+                {
+                    string tresc = File.ReadAllText("ABC\\bilet.txt");
+                    Console.WriteLine("Zawartość pliku:");
+                    Console.WriteLine(tresc);
+                }
+                else
+                {
+                    Console.WriteLine("brak pliku bilet.txt");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nie ma folderu ABC");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
